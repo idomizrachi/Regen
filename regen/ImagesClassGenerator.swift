45f119459ffcae981eec765b5727fe5fd8fcd5fa
Ido Mizrachi <ido.mizrachi@jivesoftware.com>
2017-05-10 13:38:54 +0300

    Change to static methods

--- regen/ImagesClassGenerator.swift
+++ regen/ImagesClassGenerator.swift
@@ -9,25 +9,26 @@
 import Cocoa
 
 class ImagesClassGenerator {
-    
     func generateClass(fromImages images : [ImageAssetMetadata], generatedFile : String) {
         var headerFile = ""
         var implementationFile = ""
-        
+ 
         headerFile += "@import Foundation;\n\n"
-        headerFile += "extern const struct \(generatedFile) {\n"
+        headerFile += "@interface \(generatedFile) : NSObject\n\n"
         
         implementationFile += "#import \"\(generatedFile).h\"\n"
-        implementationFile += "const struct \(generatedFile) \(generatedFile) = {\n"
+        implementationFile += "@implementation \(generatedFile)\n\n"
+        
         
         for metadata in images {
-            headerFile += "    __unsafe_unretained NSString *\(metadata.property);\n"
-            implementationFile += "    .\(metadata.property) = @\"\(metadata.imageNamed)\",\n"
-
+            headerFile += "+(NSString *) \(metadata.property);\n"
+            implementationFile += "+(NSString *) \(metadata.property) {\n"
+            implementationFile += "    return \"\(metadata.imageNamed)\";\n"
+            implementationFile += "}\n\n";
         }
-                
-        headerFile += "\n} \(generatedFile);\n"
-        implementationFile += "\n};\n"
+        
+        headerFile += "@end"
+        implementationFile += "@end"
         
         do {
             try headerFile.write(toFile: generatedFile + ".h", atomically: false, encoding: String.Encoding.utf8)
